5914089d3279a5e46eb12204621aec3065aa7230
kano521520 <2258939528@qq.com>
2026-03-29 17:32:33 +0800

    Initial Project Build

---
+++ Assets/RobotController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 7f;
+    public float jumpForce = 5f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(moveX, 0, moveZ).normalized;
+
+        rb.velocity = new Vector3(moveDirection.x * moveSpeed, rb.velocity.y, moveDirection.z * moveSpeed);
+
+        if (moveDirection != Vector3.zero)
+        {
+            transform.forward = moveDirection;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = true;
+        }
+    }
+}
